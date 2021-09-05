8f6cfbe76af87620da5baed103deadc250ccf5ae
Ren0503 <illuminati0440@gmail.com>
2021-09-05 22:52:13 +0700

    Add scroll to top

--- client/src/layouts/MainLayout.js
+++ client/src/layouts/MainLayout.js
@@ -1,15 +1,42 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Footer, Header } from 'components/core';
-import { Container } from 'react-bootstrap';
-
+import { Container, Button } from 'react-bootstrap';
 
 const MainLayout = ({ children }) => {
+    const [visible, setVisible] = useState(false);
+
+    const toggleVisibility = () => {
+        if (window.pageYOffset > 300) {
+            setVisible(true);
+        } else {
+            setVisible(false);
+        }
+    };
+
+    const scrollToTop = () => {
+        window.scrollTo({
+            top: 0,
+            behavior: "smooth"
+        });
+    };
+
+    useEffect(() => {
+        window.addEventListener("scroll", toggleVisibility);
+    }, []);
+
     return (
         <div>
             <Header />
             <Container>
                 <div className="py-3">{children}</div>
             </Container>
+            <div className="scroll-to-top">
+                {visible &&
+                    <Button className='btn btn-dark' onClick={scrollToTop}>
+                        <i className="fas fa-angle-up"></i>
+                    </Button>
+                }
+            </div>
             <Footer />
         </div>
     )
